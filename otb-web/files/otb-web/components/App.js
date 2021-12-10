5e13cc9c225b25a8f0934c3de1ed16ecf43ddfec
Raphael Westphal <raphael.westphal@ovhcloud.com>
2021-12-10 17:12:55 +0100

    otb-web: Do retry on register

--- otb-web/files/otb-web/components/App.js
+++ otb-web/files/otb-web/components/App.js
@@ -7,8 +7,10 @@ export default {
       localIpListToWan: [],
       service: '',
       tunnelStatus: '',
+      isRegistering: false,
       version: '',
       serviceRegistered: '',
+      retryCount: 0,
     };
   },
   mounted() {
@@ -17,30 +19,67 @@ export default {
   methods: {
     registerService() {
       // register service
-      this.call('POST', this.serviceRegistered);
-    },
-    initialize() {
-      this.call('GET');
-    },
-    call(method, url) {
-      fetch(`/cgi-bin/me/${url || ''}`, { method }).then((response) => response.json()).then((data) => {
-        console.log('data', data);
+      this.isRegistering = true;
+      this.call('POST', this.serviceRegistered, (data) => {
+        if (!data.service) {
+          if (this.retryCount >= 10) {
+            this.retryCount = 0;
+            this.isRegistering = false;
+
+            // Add the shake animation
+            document.getElementById("service").style.animation = "shake 0.82s cubic-bezier(.36,.07,.19,.97) both";
+            setTimeout(() => {
+              document.getElementById("service").style.animation = "";
+            }, 1000)
+            return
+          }
+
+          // Do the callback again in one second
+          this.retryCount++;
+          setTimeout(this.registerService, 1000)
+          return
+        }
+
         this.device = data.device;
         this.dns = data.dns;
         this.localIpListToWan = data.localIpListToWan;
         this.service = data.service;
         this.tunnelStatus = data.tunnelStatus;
         this.version = data.version;
-      }).catch((error) => {
+      }, (error) => {console.log(error);});
+    },
+    refreshView() {
+      this.call('GET', '', (data) => {
+        if (!data.device) {
+          this.device = 'Your device cannot reach the internet, is it properly connected ?'
+          setTimeout(this.refreshView, 1000)
+        } else {
+          this.device = data.device;
+        }
+        this.dns = data.dns;
+        this.localIpListToWan = data.localIpListToWan;
+        this.service = data.service;
+        this.tunnelStatus = data.tunnelStatus;
+        this.version = data.version;
+      }, (error) => {
         console.log(error);
       });
+    },
+    initialize() {
+      this.refreshView();
+    },
+    call(method, url, callback, error) {
+      fetch(`/cgi-bin/me/${url || ''}`, { method })
+      .then((response) => response.json())
+      .then((data) => callback(data))
+      .catch((err) => error(err));
     },
   },
   template: `
     <h1 class="c">OverTheBox</h1>
     <div class="box" v-if="typeof device === 'string'">
       <div v-if="service.length > 0">
-        <p>
+        <p class="first-paragraphe">
           <b>Service</b>
           <div class="mono">{{service}}</div>
         </p>
@@ -50,16 +89,21 @@ export default {
         </p>
       </div>
       <div v-if="service.length <= 0">
-        <p>
+        <p class="first-paragraphe">
           <b>This device is not associated with any service.</b>
           <br>
-          Please register it on the <a href="https://www.ovhtelecom.fr/manager/#/overTheBox/">manager</a>.
+          Please register it on the <a target="_blank" href="https://www.ovhtelecom.fr/manager/#/overTheBox/">manager</a>.
           <br>
           Then confirm the service ID here.
         </p>
         <div class="c">
-          <input type="text" placeholder="Enter your service ID" size="47" maxlength="47" class="c mono" id="service" v-model="serviceRegistered">
-          <button class="c btn" @click="registerService">Register</button>
+          <input type="text" placeholder="overthebox.xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx" size="47" maxlength="47" class="c mono" id="service" v-model="serviceRegistered">
+          <button v-if="isRegistering" id="button-register" class="c btn" style="background-color: #ccccf;">
+            <div style="color: transparent;" class="loading-spinner ">Register</div>
+          </button>
+          <button v-if="!isRegistering" id="button-register" class="c btn" @click="registerService">
+            Register
+          </button>
         </div>
       </div>
       <p>
@@ -70,20 +114,18 @@ export default {
     <div class="box" v-if="!(typeof device === 'string')">
       <p class="c">The OverTheBox is registering to our servers ...</p>
     </div>
-    <p>
+  <p style="margin-bottom: 0">
       <b>Local IP to WAN</b>
-        <ul>
           <p v-for="item in localIpListToWan">
             <b>{{item.name}}</b>
             <div class="ip">{{item.ip}}</div>
           </p>
-        </ul>
     </p>
-    <p>
+    <p style="padding-top: 0; margin-top: 0;">
       <b>DNS server</b>
       <div class="dns">{{dns || 'unknown dns servers'}}</div>
     </p>
     <img src="logo.png" class="logo" />
-    <div class="version">{{version || 'unknown version'}}</div>
+    <div class="version">{{version ? version.replace("-", ".") : 'unknown version'}}</div>
   `,
 };
